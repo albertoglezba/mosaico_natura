792379b47974a7fa132c976201323a29087dc78b
Carlos Alonso Torres <calonso@calonso-debian>
2014-12-18 11:10:09 -0600

    mensaje de revisar carpeta spam

--- protected/views/site/confirma.php
+++ protected/views/site/confirma.php
@@ -1,4 +1,7 @@
 <p>
 	¡Tu registro fue creado exitosamente! <br><br>El siguiente paso es confirmar tu cuenta,
 	en breve te ser&aacute; enviado el enlace de confirmaci&oacute;n a tu correo. 
+	<br><br>
+	NOTA: A veces el correo de confirmaci&oacute;n llega a la carpeta de "spam" o "correo no deseado",
+	por favor asegurate de revisarlas. 
 </p>
