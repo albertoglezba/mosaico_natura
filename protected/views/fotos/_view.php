8183e6732e9bd5fc930d4e25957d436e2de7cb9a
calonso-conabio <calonso@conabio.gob.mx>
2016-01-19 13:41:25 -0600

    Los jovenes solo pueden subir como maximo 2 fotos, parte de la vista

--- protected/views/fotos/_view.php
+++ protected/views/fotos/_view.php
@@ -5,9 +5,15 @@
 
 <div class="view">
 	
-	<h2><?php echo CHtml::encode($data->categoria->nombre) ?></h2>
-	<br><?php echo CHtml::link('[modificar la ubicación, título o marca/modelo de tu cámara fotográfica]', array('fotos/update/'.$data->id), array('style'=>'color:#BD5D28')); ?>
-	<h3><strong><em><?php echo $data->titulo; ?></em></strong></h3>
+	<?php if (empty($data->categoria_id)) { ?>
+		<h2><strong><em><?php echo $data->titulo; ?></em></strong></h2>
+		<br><?php echo CHtml::link('[modificar la ubicación, título o marca/modelo de tu cámara fotográfica]', array('fotos/update/'.$data->id), array('style'=>'color:#BD5D28')); ?>
+	<?php } else { ?>
+		<h2><?php echo CHtml::encode($data->categoria->nombre) ?></h2>
+		<br><?php echo CHtml::link('[modificar la ubicación, título o marca/modelo de tu cámara fotográfica]', array('fotos/update/'.$data->id), array('style'=>'color:#BD5D28')); ?>
+		<h3><strong><em><?php echo $data->titulo; ?></em></strong></h3>
+	<?php } ?>
+	
 	<div>
 	<?php echo CHtml::image($data->ruta, '', array("title"=>$data->titulo, "width"=>"900px")) ?>
 	</div>	
