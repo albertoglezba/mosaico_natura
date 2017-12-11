4264d573654a094c0a10c7e6f9bb8b870500e609
Gerardo Alberto González Bautista <albertoglezba@gmail.com>
2017-12-11 14:36:29 -0600

    hommogenizeacion  en los botones de subir cosos

--- protected/views/usuarios/view.php
+++ protected/views/usuarios/view.php
@@ -4,18 +4,20 @@
 ?>
 
 <h1>Informaci&oacute;n de tu cuenta</h1>
-<h4>Configurar tus datos: <?php echo CHtml::link('<span class="btn btn-link glyphicon glyphicon-cog" aria-hidden="true"></span>', Yii::app()->baseUrl."/index.php/usuarios/update/".$model->id); ?></h4>
+<?php echo CHtml::link('<span class="glyphicon glyphicon-cog" aria-hidden="true"></span> Configurar tus datos', Yii::app()->baseUrl."/index.php/usuarios/update/".$model->id, array('class'=>"btn btn-lg btn-info")); ?>
+<hr />
 <?php
 
 $this->widget ( 'zii.widgets.CDetailView', array (
 	'data' => $model,
 	'htmlOptions'=>array(
-		'class'=>'table-responsive table-condensed',
+		'class'=>'table-responsive table-bordered text-left',
 	),
 	'attributes' => array (
 		'usuario',
 		'nombre',
 		'apellido',
+		'fecha_nac',
 		'correo',
 		'telefonos',
 		'municipio',
--- protected/views/videos/index.php
+++ protected/views/videos/index.php
@@ -1,7 +1,6 @@
-<h1>Tu video</h1>
-<?php echo CHtml::link('Subir', Yii::app()->baseUrl."/index.php/videos/create", array('style'=>"color:#BD5D28;")); ?> un video
-<br><br>
-
+<h1>Tus videos</h1>
+<?php echo CHtml::link('<span class="glyphicon glyphicon-upload" aria-hidden="true"></span> Subir videos', Yii::app()->baseUrl."/index.php/videos/create", array('class'=>"btn btn-lg btn-info")); ?>
+<hr />
 <?php $this->widget('zii.widgets.CListView', array(
 	'dataProvider'=>$dataProvider,
 	'itemView'=>'_view',
