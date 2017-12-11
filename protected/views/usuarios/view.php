417c705aaac2a278b8f1ea0fa49c6f4fc1cc1012
Gerardo Alberto González Bautista <albertoglezba@gmail.com>
2017-12-11 11:34:32 -0600

    actualizando tabla y html options en ella

--- protected/views/usuarios/view.php
+++ protected/views/usuarios/view.php
@@ -3,12 +3,15 @@
 /* @var $model Usuarios */
 ?>
 
-<h1 class="text-info">Informaci&oacute;n de tu cuenta</h1>
-<h3><?php echo CHtml::link('Actualiza tus datos <span class="glyphicon glyphicon-pencil" aria-hidden="true"></span>.', Yii::app()->baseUrl."/index.php/usuarios/update/".$model->id); ?></h3>
+<h1>Informaci&oacute;n de tu cuenta</h1>
+<h4>Configurar tus datos: <?php echo CHtml::link('<span class="btn btn-link glyphicon glyphicon-cog" aria-hidden="true"></span>', Yii::app()->baseUrl."/index.php/usuarios/update/".$model->id); ?></h4>
 <?php
 
 $this->widget ( 'zii.widgets.CDetailView', array (
 	'data' => $model,
+	'htmlOptions'=>array(
+		'class'=>'table-responsive table-condensed',
+	),
 	'attributes' => array (
 		'usuario',
 		'nombre',
