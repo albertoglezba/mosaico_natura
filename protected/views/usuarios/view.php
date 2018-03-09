b9c7048c0795bed42d626b7a4c2c083b0a7c59af
Gerardo Alberto González Bautista <albertoglezba@gmail.com>
2018-03-08 23:34:15 -0600

    legibilidad al mostrar los datos

--- protected/views/usuarios/view.php
+++ protected/views/usuarios/view.php
@@ -2,41 +2,47 @@
 /* @var $this UsuariosController */
 /* @var $model Usuarios */
 ?>
+<div class="row">
+	<div class="col-sm-12">
+		<h1>Informaci&oacute;n de tu cuenta</h1>
 
-<div class="col-sm-12">
-	<h1>Informaci&oacute;n de tu cuenta</h1>
+		<?php if(isset($notice)){ ?>
+			<h4 class="alert alert-info"><?php echo $notice ?></h4>
+		<?php } ?>
 
-	<?php if(isset($notice)){ ?>
-		<h4 class="alert alert-info"><?php echo $notice ?></h4>
-	<?php } ?>
+		<hr />
+		<?php echo CHtml::link('<span class="glyphicon glyphicon-cog" aria-hidden="true"></span> Edita tus datos', Yii::app()->baseUrl."/index.php/usuarios/update/".$model->id, array('class'=>"btn btn-lg btn-info")); ?>
+	</div>
 
-	<hr />
-	<?php echo CHtml::link('<span class="glyphicon glyphicon-cog" aria-hidden="true"></span> Configurar tus datos', Yii::app()->baseUrl."/index.php/usuarios/update/".$model->id, array('class'=>"btn btn-lg btn-info")); ?>
+	<div class="col-sm-12">
+		<?php
+
+		$this->widget ( 'zii.widgets.CDetailView', array (
+			'data' => $model,
+			'htmlOptions'=>array(
+				'class'=>'table-responsive text-right text-primary',
+				'style'=>'margin: 20px auto; font-size: 2em; width: auto;'
+			),
+			'attributes' => array (
+				'usuario',
+				'nombre',
+				'apellido',
+				'fecha_nac',
+				'correo',
+				'telefonos',
+				'municipio',
+				'estado',
+				'compromiso',
+				'difusion',
+				'fec_alta'
+			)
+
+		) );
+		?>
+
+	</div>
 </div>
 
-<?php
-
-$this->widget ( 'zii.widgets.CDetailView', array (
-	'data' => $model,
-	'htmlOptions'=>array(
-		'class'=>'table-responsive table-bordered text-left',
-	),
-	'attributes' => array (
-		'usuario',
-		'nombre',
-		'apellido',
-		'fecha_nac',
-		'correo',
-		'telefonos',
-		'municipio',
-		'estado',
-		'compromiso',
-		'difusion',
-		'fec_alta'
-	)
-
-) );
-?>
 <script>
 	$("#yw0").removeClass("detail-view");
 	$("#yw0").addClass("table");
