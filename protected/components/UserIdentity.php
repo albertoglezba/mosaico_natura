65370de019d815198da921519e79ee0b790edd66
Carlos Alonso <calonsogeek@gmail.com>
2019-02-09 00:15:14 -0600

    quitando la autenticacion por usuario, solo se deja correo para mas simple

--- protected/components/UserIdentity.php
+++ protected/components/UserIdentity.php
@@ -7,70 +7,66 @@
  */
 class UserIdentity extends CUserIdentity
 {
-	private $id;
+    private $id;
 
-	/**
-	 * Authenticates a user.
-	 * The example implementation makes sure if the username and password
-	 * are both 'demo'.
-	 * In practical applications, this should be changed to authenticate
-	 * against some persistent user identity storage (e.g. database).
-	 * @return boolean whether authentication succeeds.
-	 */
-	public function authenticate()
-	{
-		$credenciales = $this->validaLogin($this->username);
-		$errores = array();
+    /**
+     * Authenticates a user.
+     * The example implementation makes sure if the username and password
+     * are both 'demo'.
+     * In practical applications, this should be changed to authenticate
+     * against some persistent user identity storage (e.g. database).
+     * @return boolean whether authentication succeeds.
+     */
+    public function authenticate()
+    {
+        $credenciales = $this->validaLogin($this->username);
+        $errores = array();
 
-		if($credenciales != NULL)
-		{
-			$users=array(
-					$this->username => $credenciales->passwd,
-					'salt' => $credenciales->salt,
-			);
-			
-			if(!isset($users[$this->username]))
-			{
-				$this->errorCode=self::ERROR_USERNAME_INVALID;
-				$errores['descripcion'] = 'La contraseña no puede ser nula.';
-			}
-			elseif($users[$this->username]!==md5($this->password."|".$users['salt']))
-			{
-			$this->errorCode=self::ERROR_PASSWORD_INVALID;
-			$errores['descripcion'] = 'Usuario o contraseña inválidos.';
-			}
-			elseif ($users[$this->username]===md5($this->password."|".$users['salt']))
-			{
-				if ($credenciales->confirmo == 0)
-				{
-					$this->errorCode=self::ERROR_UNKNOWN_IDENTITY;
-					$error = 'Primero tienes que confirmar tu cuenta con el enlace que se te envió a tu correo. ';
-					$error.= '<br><b>NOTA:</b> A veces el correo de confirmaci&oacute;n llega a la carpeta de "spam" o "correo no deseado", ';
-					$error.= 'por favor asegurate de revisarlas.';
-					$error.= "<br><br>¿No te llego el correo? <a href='".Yii::app()->createAbsoluteUrl('site/confirma')."?id=".$credenciales->id."&fec_alta=".urlencode($credenciales->fec_alta)."' target='_blank'>Enviar de nuevo el correo de confirmación</a>"; 
-					$errores['descripcion'] = $error;
-				} else
-					$this->errorCode=self::ERROR_NONE;
-			}
-			else
-				$this->errorCode=self::ERROR_NONE;
-		} else {
-			$this->errorCode=self::ERROR_USERNAME_INVALID;
-		}
-		
-		$errores['error'] = !$this->errorCode;
-		return $errores;
-	}
+        if($credenciales != NULL)
+        {
+            $users=array(
+                $this->username => $credenciales->passwd,
+                'salt' => $credenciales->salt,
+            );
 
-	/**
-	 *
-	 * @param string $usr el usuario
-	 */
-	public function validaLogin($usr)
-	{
-		$model = Usuarios::model()->findByAttributes(array('usuario'=>$usr));
-		if ($model == NULL)
-			$model = Usuarios::model()->findByAttributes(array('correo'=>$usr));
-		return $model;
-	}
+            if(!isset($users[$this->username]))
+            {
+                $this->errorCode=self::ERROR_USERNAME_INVALID;
+                $errores['descripcion'] = 'La contraseña no puede ser nula.';
+            }
+            elseif($users[$this->username]!==md5($this->password."|".$users['salt']))
+            {
+                $this->errorCode=self::ERROR_PASSWORD_INVALID;
+                $errores['descripcion'] = 'Correo o contraseña inválidos.';
+            }
+            elseif ($users[$this->username]===md5($this->password."|".$users['salt']))
+            {
+                if ($credenciales->confirmo == 0)
+                {
+                    $this->errorCode=self::ERROR_UNKNOWN_IDENTITY;
+                    $error = 'Primero tienes que confirmar tu cuenta con el enlace que se te envió a tu correo. ';
+                    $error.= '<br><b>NOTA:</b> A veces el correo de confirmaci&oacute;n llega a la carpeta de "spam" o "correo no deseado", ';
+                    $error.= 'por favor asegurate de revisarlas.';
+                    $error.= "<br><br>¿No te llego el correo? <a href='".Yii::app()->createAbsoluteUrl('site/confirma')."?id=".$credenciales->id."&fec_alta=".urlencode($credenciales->fec_alta)."' target='_blank'>Enviar de nuevo el correo de confirmación</a>";
+                    $errores['descripcion'] = $error;
+                } else
+                    $this->errorCode=self::ERROR_NONE;
+            }
+            else
+                $this->errorCode=self::ERROR_NONE;
+        } else
+            $errores['descripcion'] = 'Este correo no existe en nuestra base';
+
+        $errores['error'] = !$this->errorCode;
+        return $errores;
+    }
+
+    /**
+     *
+     * @param string $usr el usuario
+     */
+    public function validaLogin($usr)
+    {
+        return Usuarios::model()->findByAttributes(array('correo'=>$usr));
+    }
 }
--- protected/views/site/login.php
+++ protected/views/site/login.php
@@ -1,52 +1,48 @@
+<span>
+    <?php
+    if (isset($_GET['situacion']))
+        echo $_GET['situacion'];
 
-					<span>
-					<?php
-						if (isset($_GET['situacion']))
-							echo $_GET['situacion'];
-					?>
-					</span>
 
-					<h3>Ingresa tus datos, si ya habías creado una cuenta en anteriores concursos puedes ingresar con esas credenciales</h3>
+    ?>
+</span>
 
-					<div class="form">
-						<?php $form=$this->beginWidget('CActiveForm', array(
-							'id'=>'login-form',
-							'enableClientValidation'=>true,
-							'clientOptions'=>array(
-								'validateOnSubmit'=>true,
-								'class'=>'form-control'
-							),
-							//'htmlOptions'=>array('class'=>'form-control')
-						)); ?>
+<h3>Ingresa tus datos, si ya habías creado una cuenta en anteriores concursos puedes ingresar con esas credenciales</h3>
 
-						<div class="form-group">
-							<label>Usuario o correo</label>
-							<?php echo $form->textField($model,'username', array('class'=>'form-control')); ?>
-							<?php echo $form->error($model,'username'); ?>
-						</div>
+<div class="form">
+    <?php $form=$this->beginWidget('CActiveForm', array(
+        'id'=>'login-form',
+        'enableClientValidation'=>true,
+        'clientOptions'=>array(
+            'validateOnSubmit'=>true,
+            'class'=>'form-control'
+        ),
+    )); ?>
 
-						<div class="form-group">
-							<label>Contrase&ntilde;a</label>
-							<?php echo $form->passwordField($model,'password', array('class'=>'form-control')); ?>
-							<?php echo $form->error($model,'password'); ?>
+    <div class="form-group">
+        <label>Correo</label>
+        <?php echo $form->textField($model,'username', array('class'=>'form-control')); ?>
+        <?php echo $form->error($model,'username'); ?>
+    </div>
 
-						</div>
+    <div class="form-group">
+        <label>Contrase&ntilde;a</label>
+        <?php echo $form->passwordField($model,'password', array('class'=>'form-control')); ?>
+        <?php echo $form->error($model,'password'); ?>
 
-						<div class="form-group rememberMe">
-							<label for="LoginForm_rememberMe">Recordarme la próxima vez</label>
-							<?php echo $form->checkBox($model,'rememberMe'); ?>
-							<?php echo $form->error($model,'rememberMe'); ?>
-						</div>
+    </div>
 
-						<!-- div>
-							¿Has olvidado tu <a href="<?php //echo Yii::app()->baseUrl."/index.php/usuarios/reset_passwd"; ?>">contrase&ntilde;a</a>?
-						</div-->
+    <div class="form-group rememberMe">
+        <label for="LoginForm_rememberMe">Recordarme la próxima vez</label>
+        <?php echo $form->checkBox($model,'rememberMe'); ?>
+        <?php echo $form->error($model,'rememberMe'); ?>
+    </div>
 
-						<div class="buttons">
-							<?php echo CHtml::submitButton('Entra', array('class'=>'form-control btn btn-primary')); ?>
-						</div>
+    <div class="buttons">
+        <?php echo CHtml::submitButton('Entra', array('class'=>'form-control btn btn-primary')); ?>
+    </div>
 
-						<p>¿No recuerdas tu contraseña? Sigue este <?php echo CHtml::link("enlace", Yii::app()->request->baseUrl."/index.php/site/recupera"); ?></p>
-						<?php $this->endWidget(); ?>
-					</div><!-- form -->
+    <p>¿No recuerdas tu contraseña? Sigue este <?php echo CHtml::link("enlace", Yii::app()->request->baseUrl."/index.php/site/recupera"); ?></p>
+    <?php $this->endWidget(); ?>
+</div>
 			
